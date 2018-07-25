22b583d4ebd04bef05d4d9e2b41bf8d6b77202b7
unknown <jskyers@spartaglobal.com>
2018-07-25 17:48:21 +0100

    7 roots defined and connected

---
+++ one_piece/app/controllers/characters_controller.rb
@@ -0,0 +1,49 @@
+class CharactersController < ApplicationController
+  def index
+
+  end
+  
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @character = Character.find(params[:id])
+
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :power, :crew_id)
+    end
+end
