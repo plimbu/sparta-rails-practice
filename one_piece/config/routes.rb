e5bef4f541fd40fc1a8a484a862b3ca241970f0d
unknown <jskyers@spartaglobal.com>
2018-07-26 09:24:10 +0100

    created index page for database

--- one_piece/config/routes.rb
+++ one_piece/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'characters/index'
   resources :characters
+
+  root 'characters#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
